303dca98cbab3a9877f78ea9faf96cbe41cceb90
dracc <ssrinet10@gmail.com>
2024-07-19 00:21:59 +0530

    made the basics

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Is this truly anonymous?",
+    user: "Dracc",
+    added: new Date()
+  },
+  {
+    text: "Let's Check",
+    user: "idk",
+    added: new Date()
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini MessageBoard',messages: messages });
+});
+
+
+router.get('/new',function(req,res,next){
+  res.render('form');
+})
+
+router.post('/new',function(req,res,next){
+  messages.push({text:req.body.text,user:req.body.user,added:new Date()})
+  res.redirect('/');
+})
+
+module.exports = router;
